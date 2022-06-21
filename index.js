4d62de9c2c726046a45d3e321b28e946b709207f
Justin James <justinmjames@protonmail.com>
2022-06-21 15:17:22 -0700

    commit tree b99f98668ba1a781b18077774fed609a39c998ca

--- index.js
+++ index.js
@@ -8,6 +8,61 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (arrayRec) => {
+    return arrayRec.map(record => createEmployeeRecord(record))
+}
+
+const createTimeInEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    const inEvent = {
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    this.timeInEvents.push(inEvent)
+    return this
+}
+
+const createTimeOutEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    const outEvent = {
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date
+    }
+    this.timeOutEvents.push(outEvent)
+    return this
+}
+
+const hoursWorkedOnDate = function(targetDate) {
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const OutEvent = this.timeOutEvents.find(inEvent => inEvent.date === targetDate)
+    return(OutEvent.hour - inEvent.hour) / 100
+
+}
+
+const wagesEarnedOnDate = function(targetDate) {
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(emp => emp.firstName === firstNameString)
+}
+
+function calculatePayroll(employeeRecords) {
+    return employeeRecords.map(employee => allWagesFor.call(employee)).reduce((currentValue, total) => currentValue + total)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
